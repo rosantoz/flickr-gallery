67a14f0bf23391b6617a29567480e68ee042b78c
Rodrigo <email@rodrigodossantos.ws>
2013-09-23 21:24:34 +1000

    code identation

--- public/js/app.js
+++ public/js/app.js
@@ -8,20 +8,8 @@ $(document).ready(function () {
     });
 
     $(".thumbnails").on('click', '.thumbnail', function () {
-
-        // OK -- request photo details
-        // OK -- on success open a modal window with photos details
-        // OK -- include name of the photographer
-        // OK -- include link on photo to open it in a new page
-        // OK -- include link to photographer profile http://www.flickr.com/people/{user-id}
-        // document php class
-        // unit tests
-        // publish
-
         var photoId = $(this).data('photoid');
-
         Gallery.photoDetails(photoId);
-
     });
 
 });
